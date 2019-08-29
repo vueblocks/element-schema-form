0a74c03c62e4ada86d98127566a8cb5449cc324e
xiaoluoboding <xiaoluoboding@gmail.com>
2019-08-29 17:50:24 +0800

    feat: 新增列排序功能

---
+++ app/views/form-generator/aside-panel/index.vue
@@ -0,0 +1,212 @@
+<template>
+  <el-card class="aside-layout">
+    <label for="aside-layout" class="aside-layout__label">选择布局</label>
+    <el-select v-model="activeLayout" placeholder="请选择" @change="handleChangeLayout">
+      <el-option
+        v-for="(layout, index) in rowLayouts"
+        :key="index"
+        :label="index+''"
+        :value="layout">
+        <svg fill="#BEC9D6">
+          <template v-for="(item, idx) in layout">
+            <rect
+              :key="idx"
+              :width="`${getRectWidth(layout)}%`"
+              height="20"
+              y="5"
+              rx="5"
+              ry="5"
+              :x="idx === 0 ? '0%' : `${(getRectWidth(layout) + 3) * idx}%`"
+            />
+          </template>
+        </svg>
+      </el-option>
+    </el-select>
+    <div class="aside-layout__colitems-wrap">
+      <draggable
+        v-model="colitemsGroup"
+        class="aside-layout__colitems-group"
+        tag="ul"
+        :move="checkMove"
+        v-bind="dragOptions"
+        @start="dragging = true"
+        @end="dragging = false"
+      >
+        <transition-group type="transition" :name="!dragging ? 'flip-list' : null">
+          <li
+            class="aside-layout__colitem"
+            v-for="(element) in colitemsGroup"
+            :key="element.prop"
+          >
+            <span>{{ element.prop }}</span>
+            <div class="colitem-icon colitem-icon--delete" @click="handleRemoveColumn(element)">
+              <svg viewBox="0 0 28 28" preserveAspectRatio="xMidYMid meet" shape-rendering="geometricPrecision">
+                <g><path d="M19 9h-3V8a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v1H9a1 1 0 1 0 0 2h10a1 1 0 0 0 .004-2H19zM9 20c.021.543.457.979 1 1h8c.55-.004.996-.45 1-1v-7H9v7zm2.02-4.985h2v4h-2v-4zm4 0h2v4h-2v-4z" fill-rule="evenodd"></path></g>
+              </svg>
+            </div>
+          </li>
+        </transition-group>
+        <div
+          slot="footer"
+          class="aside-layout__colitem-action"
+          role="group"
+          key="footer"
+        >
+          <div class="colitem-action__btn">
+            <el-button type="primary" size="mini" icon="el-icon-plus" @click="handleAddColumn" circle></el-button>
+          </div>
+          <div class="colitem-action__label"><span>新增列</span></div>
+        </div>
+      </draggable>
+    </div>
+  </el-card>
+</template>
+
+<script>
+import Draggable from 'vuedraggable'
+
+export default {
+  name: 'AsidePanel',
+  components: {
+    Draggable
+  },
+  inject: ['fg'],
+  data () {
+    return {
+      // aside
+      activeSection: 0,
+      activeLayout: [],
+      layouts: [
+        {
+          idx: 0,
+          span: [24]
+        },
+        {
+          idx: 1,
+          span: [12, 12]
+        }
+      ],
+      rowLayouts: [
+        [24],
+        [12, 12],
+        [8, 8, 8],
+        [6, 6, 6, 6],
+        [5, 5, 5, 5, 5],
+        [4, 4, 4, 4, 4, 4],
+        [9, 15],
+        [15, 9],
+        [8, 16],
+        [16, 8],
+        [6, 18],
+        [18, 6],
+        [6, 12, 6],
+        [4, 16, 4],
+        [6, 6, 12],
+        [12, 6, 6],
+        [4, 4, 16],
+        [16, 4, 4],
+        [4, 4, 4, 12],
+        [12, 4, 4, 4]
+      ],
+      dragging: false,
+      dragOptions: {
+        animation: 200,
+        group: 'colitem',
+        disabled: false,
+        ghostClass: 'ghost'
+      }
+    }
+  },
+  computed: {
+    colitemsGroup: {
+      get  () {
+        return this.fg.layoutSections[this.activeSection]
+      },
+      set (val) {
+        this.fg.layoutSections.splice(this.activeSection, 1, val)
+      }
+    }
+  },
+  methods: {
+    getRectWidth (item) {
+      const len = item.length
+      return (100 - len * 3) / len
+    },
+    handleChangeLayout (data) {
+      const initVal = data.map((val, idx) => {
+        return {
+          colGrid: { span: val },
+          isCustom: 'btn-addCol'
+        }
+      })
+      this.fg.layoutSections.push(initVal)
+    },
+    checkMove (e) {
+      console.log('Future index: ' + e.draggedContext.futureIndex)
+    },
+    handleAddColumn () {
+      // TODO 处理添加列业务逻辑
+      console.log('add column')
+    },
+    handleRemoveColumn (element) {
+      // TODO 处理删除列业务逻辑
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.aside-layout {
+  height: 800px;
+  &__label {
+    display: block;
+    margin-bottom: 5px;
+    font-size: 14px;
+  }
+  &__colitems-wrap {
+    margin-top: 20px;
+  }
+  &__colitem {
+    cursor: move;
+    list-style: none;
+    background: #f1f5f9;
+    font-size: 13px;
+    // min-height: 40px;
+    font-weight: 600;
+    color: #32373c;
+    margin-bottom: 10px;
+    border-radius: 3px;
+    padding: 10px;
+    position: relative;
+    text-align: center;
+    line-height: 1.5em;
+    .colitem-icon {
+      display: inline-block;
+      position: absolute;
+      right: 16px;
+      transition: fill .2s ease;
+      cursor: pointer;
+    }
+    .colitem-icon--delete {
+      fill: #737e89;
+      width: 28px;
+      min-width: 28px;
+      height: 28px;
+      margin: -6px;
+    }
+  }
+  &__colitem-action {
+    display: flex;
+    flex-direction: column;
+    font-size: 13px;
+    align-items: center;
+    .colitem-action__label {
+      padding: 8px 16px;
+    }
+  }
+}
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+</style>
--- app/views/form-generator/index.vue
+++ app/views/form-generator/index.vue
@@ -1,30 +1,7 @@
 <template>
   <div class="form-generator">
     <aside class="form-generator__aside">
-      <el-card class="aside-layout">
-        <label for="aside-layout" class="aside-layout__label">选择布局</label>
-        <el-select v-model="activeLayout" placeholder="请选择" @change="handleChangeLayout">
-          <el-option
-            v-for="(layout, index) in layouts"
-            :key="index"
-            :label="index+''"
-            :value="layout">
-            <svg fill="#BEC9D6">
-              <template v-for="(item, idx) in layout">
-                <rect
-                  :key="idx"
-                  :width="`${getRectWidth(layout)}%`"
-                  height="20"
-                  y="5"
-                  rx="5"
-                  ry="5"
-                  :x="idx === 0 ? '0%' : `${(getRectWidth(layout) + 3) * idx}%`"
-                />
-              </template>
-            </svg>
-          </el-option>
-        </el-select>
-      </el-card>
+      <aside-panel />
     </aside>
     <main class="form-generator__main">
       <el-card class="main-layout">
@@ -60,7 +37,7 @@
                   class="btn-removeCol"
                   type="primary"
                   size="mini"
-                  icon="el-icon-close"
+                  icon="el-icon-delete"
                   circle
                   @click="handleRemoveComponent(scope)"
                 />
@@ -79,11 +56,13 @@
 </template>
 
 <script>
+import AsidePanel from './aside-panel'
 import ConfigPanel from './config-panel'
 
 export default {
   name: 'FormGenerator',
   components: {
+    AsidePanel,
     ConfigPanel
   },
   provide () {
@@ -93,30 +72,6 @@ export default {
   },
   data () {
     return {
-      // aside
-      activeLayout: [],
-      layouts: [
-        [24],
-        [12, 12],
-        [8, 8, 8],
-        [6, 6, 6, 6],
-        [5, 5, 5, 5, 5],
-        [4, 4, 4, 4, 4, 4],
-        [9, 15],
-        [15, 9],
-        [8, 16],
-        [16, 8],
-        [6, 18],
-        [18, 6],
-        [6, 12, 6],
-        [4, 16, 4],
-        [6, 6, 12],
-        [12, 6, 6],
-        [4, 4, 16],
-        [16, 4, 4],
-        [4, 4, 4, 12],
-        [12, 4, 4, 4]
-      ],
       // main
       layoutSections: [],
       basicComponents: [
@@ -137,42 +92,7 @@ export default {
           name: '日期控件'
         }
       ],
-      formModel: {},
-      formOptions: {},
-      formLayout: {
-        gutter: 16
-      },
-      formSettings: {
-        labelPosition: 'left',
-        size: 'small'
-      }
-    }
-  },
-  methods: {
-    getRectWidth (item) {
-      const len = item.length
-      return (100 - len * 3) / len
-    },
-    handleChangeLayout (data) {
-      console.log(data)
-      console.log(this.$slots)
-      const initVal = data.map((val, idx) => {
-        return {
-          colGrid: { span: val },
-          isCustom: 'btn-addCol'
-        }
-      })
-      this.layoutSections.push(initVal)
-    },
-    handleAddComponent (comp, scope) {
-      const { col, rowIndex, colIndex } = scope
-      console.group('---- ---- ----')
-      console.log(comp)
-      console.log(col)
-      console.log('row', rowIndex)
-      console.log('col', colIndex)
-      console.groupEnd()
-      const formater = [
+      formFormater: [
         {
           type: 'input',
           prop: 'name',
@@ -199,8 +119,28 @@ export default {
             'end-placeholder': '结束月份'
           }
         }
-      ]
-      const component = formater.find(v => comp.type === v.type)
+      ],
+      formModel: {},
+      formOptions: {},
+      formLayout: {
+        gutter: 16
+      },
+      formSettings: {
+        labelPosition: 'left',
+        size: 'small'
+      }
+    }
+  },
+  methods: {
+    handleAddComponent (comp, scope) {
+      const { col, rowIndex, colIndex } = scope
+      console.group('---- ---- ----')
+      console.log(comp)
+      console.log(col)
+      console.log('row', rowIndex)
+      console.log('col', colIndex)
+      console.groupEnd()
+      const component = this.formFormater.find(v => comp.type === v.type)
       const oldSection = this.layoutSections[rowIndex][colIndex]
       const newSection = {
         ...component,
@@ -323,14 +263,6 @@ export default {
   width: 100%;
   &__aside {
     width: 240px;
-    .aside-layout {
-      height: 800px;
-      .aside-layout__label {
-        display: block;
-        margin-bottom: 5px;
-        font-size: 14px;
-      }
-    }
   }
   &__main {
     margin-left: 20px;
@@ -362,6 +294,12 @@ export default {
     .config-panel-layout {
       height: 800px;
     }
+  }
+  .flip-list-move {
+    transition: transform 0.5s;
+  }
+  .no-move {
+    transition: transform 0s;
   }
 }
 </style>
